d79bedab85dcbd2de756dfad04092941dca26d39
Andrew <a9507652513@gmail.com>
2023-01-11 22:25:27 +0300

    add flask

--- fmain.py
+++ fmain.py
@@ -4,12 +4,12 @@ import git
 app = Flask(__name__)
 
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('https://github.com/Andrewindykov/Markov-bot2.git')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('https://github.com/Andrewindykov/Markov-bot2.git')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
